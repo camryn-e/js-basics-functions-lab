a3447609d7989d6e24b9656f8b2993f8ef944e4a
camryn williams <williams.camryn.e@gmail.com>
2021-03-17 16:49:12 -0400

    commit tree b72e27add5d5165546c4c1aa6f8717003ef92f43

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickup){
+    let distance;
+    if(pickup < 42){
+        distance = 42 - pickup;
+    } else {
+        distance = pickup - 42
+    }
+    return distance;
+}
+
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(begin, dest){
+    let dist;
+    if(begin > dest){
+        dist = (begin - dest) * 264;
+    } else {
+        dist = (dest - begin) * 264;
+    }
+    return dist;
+}
+
+function calculatesFarePrice(begin, dest){
+    let dist = distanceTravelledInFeet(begin, dest);
+    let price;
+    if (dist < 400) {
+        price = 0;
+    } else if (dist > 400 && dist <= 2000){
+        price = (dist - 400) * 0.02;
+    } else if (dist > 2000 && dist < 2500){
+        price = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+
+    return price;
+}
